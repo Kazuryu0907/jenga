20183a94d314db3344f8167043fa96bd5b361b99
kazuryu <41988990+Kazuryu0907@users.noreply.github.com>
2024-04-18 16:25:46 +0900

    :sparkles: add ticket_number col

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import { Hono } from 'hono'
 import { customers } from './api/customer.route'
-import { prismaWithPulse } from "./api/prisma";
 import { times } from './api/time.route';
 
 const app = new Hono()
 app.get('/', async (c) => {
-  const subscription = await prismaWithPulse.customer.subscribe();
-  for await (const event of subscription){
-    console.log("New Event",event);
-  }
   return c.text('Hello Hono!')
 })
 
