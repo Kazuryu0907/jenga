684eafa9018171e79f6e0ea974cdd85c2af8d4c8
kazuryu <41988990+Kazuryu0907@users.noreply.github.com>
2024-04-20 19:05:54 +0900

    :sparkles: modal追加
    あとはデザイン
    アニメーションとか，formの内容とかも表示させたい

--- src/app/add/Success.tsx
+++ src/app/add/Success.tsx
@@ -1,17 +1,18 @@
 
 
-export  function Success() {
+export  function Success({ticketNumber}:{ticketNumber:number|undefined}) {
   return (
     <div>
       <section className="bg-gray-50">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+        <div className="flex flex-col items-center justify-center mx-auto">
+          <div className="w-full bg-white rounded-lg shadow ">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                受付番号
+                Register Complete🎉
               </h1>
               <div>
-                いろいろHere
+                <p className="text-lg font-normal text-gray-500 lg:text-xl">Ticket Number</p>
+                <div className="mt-6 flex justify-center"><span className="text-6xl font-extrabold text-blue-600">{11}</span></div>
               </div>
             </div>
           </div>
