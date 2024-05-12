a4ea60902e4cb66196d2df821c52650317736b1e
kazuryu <41988990+Kazuryu0907@users.noreply.github.com>
2024-05-12 15:49:28 +0900

    no cache追加

--- src/app/add/layout.tsx
+++ src/app/add/layout.tsx
@@ -1,6 +1,7 @@
 import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 // MantineのNotificationを使いたいがためのLayout
+export const fetchCache = "only-no-store";
 export default function Layout({children}:{children:React.ReactNode}){
     return (
         <MantineProvider>
