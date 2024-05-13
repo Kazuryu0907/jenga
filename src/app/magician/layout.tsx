645f5abfe62baa621b8d8fb85cf9383174702985
kazuryu <41988990+Kazuryu0907@users.noreply.github.com>
2024-05-13 13:24:08 +0900

    add dynamic

--- src/app/magician/layout.tsx
+++ src/app/magician/layout.tsx
@@ -2,6 +2,8 @@ import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 
 export const fetchCache = "only-no-store";
+export const dynamic = "force-dynamic";
+
 export default function Layout({children}:{children:React.ReactNode}){
     return(
         <MantineProvider>
